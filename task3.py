ff411d379625c944387dcfe77dc2d2d02ac9fb7f
Tyler Swystun <86743463+Awesome012@users.noreply.github.com>
2024-04-17 13:34:19 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,25 +1,51 @@
 import random
 
-cards = []
+cards = {}
+cards2 = []
 cardranks = ["2 of ", "3 of ", "4 of ", "5 of ", "6 of ", "7 of ", "8 of ", "9 of ", "10 of ", "Jack of ", "Queen of ", "King of ","Ace of "]
 cardsuits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 players = {
-    "player1" : {},
-    "player2" : {},
-    "player3" : {},
-    "player4" : {}
+    "player1" : [],
+    "player2" : [],
+    "player3" : [],
+    "player4" : []
+    }
+final = {
+    "player1" : [],
+    "player2" : [],
+    "player3" : [],
+    "player4" : []
     }
 
+check = 0 
 for x in cardsuits:
     for z in cardranks:
-        cards.append(f"{z}{x}")
+        cards[f"{z}{x}"] = check
+        check = check + 1
+#print(cards)
 
-random.shuffle(cards)
-print(cards)
+for x in cards:
+    cards2.append(x)
+#print(cards2)
+
+random.shuffle(cards2)
+#print(cards2)
 
 for x in range(5):
     for z in players:
-        check = players.index('player1')
-        players[f'{x}'].append("0")
+        players[f"{z}"].append(cards2[0])
+        cards2.pop(0)
+#print(cards2)
 
-print(players)
+for x in players:
+    for z in players[x]:
+        print(z)
+        print(cards[z])
+
+
+    print("\n")
+
+    
+
+#for x in players:
+    #print(players[x])
