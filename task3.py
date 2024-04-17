562f4f8e2ca26f64d69dccef589307e2306c309a
Tyler Swystun <86743463+Awesome012@users.noreply.github.com>
2024-04-17 12:29:35 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,6 +1,25 @@
 import random
 
-cards = ["c2","c3","c4","c5","c6","c7","c8","c9","c10","cJ","cQ","cK","cA", "d2","d3","d4","d5","d6","d7","d8","d9","d10","dJ","dQ","dK","dA", "h2","h3","h4","h5","h6","h7","h8","h9","h10","hJ","hQ","hK","hA", "s2","s3","s4","s5","s6","s7","s8","s9","s10","sJ","sQ","sK","sA",]
+cards = []
+cardranks = ["2 of ", "3 of ", "4 of ", "5 of ", "6 of ", "7 of ", "8 of ", "9 of ", "10 of ", "Jack of ", "Queen of ", "King of ","Ace of "]
+cardsuits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+players = {
+    "player1" : {},
+    "player2" : {},
+    "player3" : {},
+    "player4" : {}
+    }
+
+for x in cardsuits:
+    for z in cardranks:
+        cards.append(f"{z}{x}")
 
 random.shuffle(cards)
 print(cards)
+
+for x in range(5):
+    for z in players:
+        check = players.index('player1')
+        players[f'{x}'].append("0")
+
+print(players)
