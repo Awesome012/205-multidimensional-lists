f739a0f131fb08627e5e01d0a75e7f8aae44c618
Tyler Swystun <86743463+Awesome012@users.noreply.github.com>
2024-04-19 12:39:43 -0700

    finished

--- task3.py
+++ task3.py
@@ -5,10 +5,10 @@ cards = {}
 cardranks = ["2 of ", "3 of ", "4 of ", "5 of ", "6 of ", "7 of ", "8 of ", "9 of ", "10 of ", "Jack of ", "Queen of ", "King of ","Ace of "]
 cardsuits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 players = {
-    "player1" : {},
-    "player2" : {},
-    "player3" : {},
-    "player4" : {}
+    "Player 1" : {},
+    "Player 2" : {},
+    "Player 3" : {},
+    "Player 4" : {}
     }
 
 check = 0 
@@ -16,20 +16,13 @@ for x in cardsuits:
     for z in cardranks:
         cards[f"{z}{x}"] = check
         check = check + 1
-#print(cards)
 
 temp = list(cards.values())
 random.shuffle(temp)
 test = dict(zip(cards, temp))
 final = dict(sorted(test.items(), key=lambda item: item[1]))
-#print(final)
-
-for x in final:
-    #print(x)
-    continue
 cool = list(cards.keys())[0]
-#print(cool)
-#print(cards)
+
 for x in range(5):
     for z in players:
         cool = list(final.keys())[0]
@@ -38,24 +31,15 @@ for x in range(5):
         del final[cool]
         del cards[cool]
         players[z][cool] = nice
-#print(players)
-for x in players:
-    #print(players[x])
-    continue
-#print(players)
-players.sort()
-#print(cards)
 
 for x in players:
+    players[x] = sorted(players[x].items(), key=lambda x:x[1])
+    players[x] = dict(players[x])
+
+
+for x in players:
+    print(f"{x}:")
     for z in players[x]:
         print(z)
         continue
-
-
-    print("\n")
-
-#print(cards)
-
-
-#for x in players:
-    #print(players[x])
+    print("")
